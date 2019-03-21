86d12e9074faa8fd0b4c02c288de18648111719e
Anthea Merton <anthea.merton@gmail.com>
2019-03-21 10:34:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(numbers)
   # your code here
-  numbers=[1,2,3]
-  array.push(element)
+  new_array=[]
+  new_array.push(element)
   numbers.each do |element| 
     element **2
     numbers +=1 
